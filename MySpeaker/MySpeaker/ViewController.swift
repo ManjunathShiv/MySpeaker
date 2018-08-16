9dfd43cb902d0aba96188eb1df25cc1ca45d3bc2
ManjunathShiv <32102571+ManjunathShiv@users.noreply.github.com>
2018-08-16 09:43:16 +0530

    Add files via upload

---
+++ MySpeaker/MySpeaker/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MySpeaker
+//
+//  Created by Manjunath Shivakumara on 14/08/18.
+//  Copyright © 2018 Manjunath Shivakumara. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let string = "Hello, World!"
+        let utterance = AVSpeechUtterance(string: string)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-UK")
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        
+//        synthesizer.pauseSpeaking(at: .word)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
